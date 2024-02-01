02e66f0de430305d7dfbf8a4aaa3090ddadbd59a
radhoda <rod.hoda@gmail.com>
2024-01-31 16:03:26 -0800

    - Added a secondary file to allow user to contact via email. - Added the description, image, and placeholder url for each project - Updated data.csv to include one personal project as a placeholder

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 import streamlit as st
-import pandas
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images\photo.png")
+    st.image("/Users/radhoda/PycharmProjects/PortfolioWebsite/images/photo.png")
 
 
 with col2:
     st.title("Rad Hoda")
     content = """
-    Hi, I am Rad! I am a Software Engineering Student at University of 
+    Hello, My name is Rad! I am a Software Engineering Student at University of 
     Washington Bothell. I am aiming to graduate in 2024, and have a 
     passion for Machine Learning and Data Science. 
     """
 
     st.info(content)
 
-    st.write("Below you can find some of the apps I have built with Python and C++."
-             "Feel free to contact me!")
+    st.write("Below you can find some of the apps I have built. Feel free to contact me.")
 
     
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
-df = pandas.read_csv(r"data.csv", sep=";")
+df = pd.read_csv(r"data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
