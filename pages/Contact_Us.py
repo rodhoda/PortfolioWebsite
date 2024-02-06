d3ecb8cf7d8fcb7f912755042f804df380b18081
radhoda <rod.hoda@gmail.com>
2024-02-05 23:32:00 -0800

    Contact Us page in the website is now able to send emails from users to personal email.

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Us")
 
@@ -6,7 +7,7 @@ st.header("Contact Us")
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address:")
     message = st.text_area("Your message: ")
+    message = message + '\n' + user_email
     button = st.form_submit_button("Submit")
     if button:
-        message = message + user_email
-        print("I was pressed!")
+        send_email(message)
--- send_email.py
+++ send_email.py
@@ -1,20 +1,15 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
-username = "rod.hoda12live.co.uk@gmail.com"
-password = "wyrmzfdsbzxpryxf"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "rod.hoda12live.co.uk@gmail.com"
+    password = "wyrmzfdsbzxpryxf"
 
-reciever = "rod.hoda12live.co.uk@gmail.com"
-context = ssl.create_default_context()
+    receiver = "rod.hoda12live.co.uk@gmail.com"
+    context = ssl.create_default_context()
 
-message = """\
-Subject: Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, reciever, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
