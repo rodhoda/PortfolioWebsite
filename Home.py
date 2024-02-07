fe220e3bfe7d82dfc223cbf0956df0aa4f142341
Rad Hoda <57790085+rodhoda@users.noreply.github.com>
2024-02-07 11:18:16 -0800

    Update Home.py
    Corrected file path to work through github and not my own personal directory

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("/Users/radhoda/PycharmProjects/PortfolioWebsite/images/photo.png")
+    st.image("/images/photo.png")
 
 
 with col2:
